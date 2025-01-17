ad6c1d1377d0f6ff68d88562f2b05d048fb66a2f
Khoa Hoang <khoahoang1698@gmail.com>
2025-01-16 16:15:40 -0800

    update terraform for dynamo

--- terraform/main.tf
+++ terraform/main.tf
@@ -10,44 +10,4 @@ resource "aws_dynamodb_table" "orbwatcher" {
     type = "S"
   }
 
-  attribute {
-    name = "currency-name"
-    type = "S"
-  }
-
-  attribute {
-    name = "price-value"
-    type = "N"
-  }
-
-  attribute {
-    name = "exchange-price-value"
-    type = "N"
-  }
-
-  global_secondary_index {
-    name            = "CurrencyNameIndex"
-    hash_key        = "currency-name"
-    projection_type = "ALL"
-    read_capacity   = 10
-    write_capacity  = 10
-  }
-
-  global_secondary_index {
-    name            = "PriceValueIndex"
-    hash_key        = "price-value"
-    projection_type = "ALL"
-    read_capacity   = 10
-    write_capacity  = 10
-  }
-
-  global_secondary_index {
-    name            = "ExchangePriceValueIndex"
-    hash_key        = "exchange-price-value"
-    projection_type = "ALL"
-    read_capacity   = 10
-    write_capacity  = 10
-  }
 }
-
-

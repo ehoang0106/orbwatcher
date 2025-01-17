3fb56727f22c23bc35d10316a960aa5cb4651ffd
Khoa Hoang <khoahoang1698@gmail.com>
2025-01-16 17:14:02 -0800

    update...

--- bot.py
+++ bot.py
@@ -15,7 +15,7 @@ bot = commands.Bot(command_prefix=PREFIX)
 @bot.event
 async def on_ready():
     print(f'{bot.user} has connected to Discord!')
-    await bot.change_presence(activity=discord.Game(name="Path of Exile 2"))
+    await bot.change_presence(activity=discord.Game(name="Path of Exile 2 | -help"))
     send_price_updates.start()
     
 

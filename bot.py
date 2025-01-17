42431a580f7ecb8e72d67a6420177d48a6ce6dc9
Khoa Hoang <khoahoang1698@gmail.com>
2025-01-17 08:08:12 -0800

    optimize the code by creating a reuse function

--- bot.py
+++ bot.py
@@ -16,7 +16,7 @@ bot = commands.Bot(command_prefix=PREFIX)
 async def on_ready():
     print(f'{bot.user} has connected to Discord!')
     await bot.change_presence(activity=discord.Game(name="Path of Exile 2 | -help"))
-    send_price_updates.start()
+    #send_price_updates.start()
     
 
 @tasks.loop(hours=3)
@@ -50,47 +50,38 @@ async def start(ctx):
 async def stop(ctx):
   await ctx.send("Bot stopped tracking prices")
   send_price_updates.stop()
+  
+  
+async def get_price(ctx, type):
+  await ctx.send("Fetching prices...")
+  data = search_prices(type)
+  message = ""
+  for item in data:
+      part = f"<:{item['formatted_currency_name']}:{item['emoji_id']}> **{item['currency_name']}**: <:ExaltedOrb:1328816616854523924> **`{item['price_value']}`** -> <:{item['formatted_currency_name']}:{item['emoji_id']}> **`{item['exchange_price_value']}`**\n"
+      if len(message) + len(part) > 2000:
+          await ctx.send(message)
+          message = part
+      else:
+          message += part
 
+  if message:
+      await ctx.send(message)
+  await ctx.send(f"Last update: {datetime.now().strftime('%Y-%m-%d %H:%M:%S')}")
+  
 @bot.command(name='price', aliases=['p'], help="Get the latest prices of currency")
-async def price(ctx):
-  await ctx.send("Fetching prices...")
-  type = "currency"
-  data = search_prices(type)
-  message = ""
-  for item in data:
-      part = f"<:{item['formatted_currency_name']}:{item['emoji_id']}> **{item['currency_name']}**: <:ExaltedOrb:1328816616854523924> **`{item['price_value']}`** -> <:{item['formatted_currency_name']}:{item['emoji_id']}> **`{item['exchange_price_value']}`**\n"
-      if len(message) + len(part) > 2000:
-          await ctx.send(message)
-          message = part
-      else:
-          message += part
-
-  if message:
-      await ctx.send(message)
-  await ctx.send(f"Last update: {datetime.now().strftime('%Y-%m-%d %H:%M:%S')}")
-
-
+async def price_currency(ctx, type="currency"):
+  await get_price(ctx, type)
+  
 @bot.command(name='waystones', aliases=['w'], help="Get the latest prices of waystones")
-async def price(ctx):
-  await ctx.send("Fetching prices...")
-  type = "waystones"
-  data = search_prices(type)
-  message = ""
-  for item in data:
-      part = f"<:{item['formatted_currency_name']}:{item['emoji_id']}> **{item['currency_name']}**: <:ExaltedOrb:1328816616854523924> **`{item['price_value']}`** -> <:{item['formatted_currency_name']}:{item['emoji_id']}> **`{item['exchange_price_value']}`**\n"
-      if len(message) + len(part) > 2000:
-          await ctx.send(message)
-          message = part
-      else:
-          message += part
+async def price_waystones(ctx, type="waystones"):
+  await get_price(ctx, type)
 
-  if message:
-      await ctx.send(message)
-  await ctx.send(f"Last update: {datetime.now().strftime('%Y-%m-%d %H:%M:%S')}")
-
+  
+  
+  
 @bot.event
 async def on_command_error(ctx, error):
-    if isinstance(error, commands.CommandNotFound):
-        await ctx.send("❌ Invalid command. Please use **`-help`** for a list of available commands.")
+  if isinstance(error, commands.CommandNotFound):
+    await ctx.send("❌ Invalid command. Please use **`-help`** for a list of available commands.")
 
 bot.run(TOKEN)

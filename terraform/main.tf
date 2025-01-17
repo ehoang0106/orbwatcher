8bf40d1b82d05a4977b65997fd84cc8ce7fb12b5
Khoa Hoang <khoahoang1698@gmail.com>
2025-01-17 11:06:56 -0800

    update hash_key and range_key

--- terraform/main.tf
+++ terraform/main.tf
@@ -3,11 +3,16 @@ resource "aws_dynamodb_table" "orbwatcher" {
   billing_mode   = "PROVISIONED"
   read_capacity  = 10
   write_capacity = 10
-  hash_key       = "date"
+  hash_key       = "currency_id"
+  range_key = "date"
+
+  attribute {
+    name = "currency_id"
+    type = "S"
+  }
 
   attribute {
     name = "date"
     type = "S"
   }
-
 }
